4dff425f5e764c01eb6b06a79d9c512dff8bb0d9
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-11-14 14:16:33 +0100

    completing second task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+1. Records all tasks that are owned by this employee
+2. Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+3. File name must be: USER_ID.csv
+"""
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    i = 0
+    params = {"id": argv[1]}
+    req = requests.get("https://jsonplaceholder.typicode.com/users", params=params)
+    name = req.json()[0].get("username")
+    req = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": argv[1]})
+
+    with open('{}.csv'.format(argv[1]), 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        while i < len(req.json()):
+            title = req.json()[i].get("title")
+            status = req.json()[i].get("completed")
+            writer.writerow([argv[1], name, status, title])
+            i += 1
