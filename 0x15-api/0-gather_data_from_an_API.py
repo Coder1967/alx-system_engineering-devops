87a954286fca2f8ce16fb7dc226f8c7b246d91af
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-11-14 13:31:13 +0100

    completing first task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,25 +5,26 @@ output the employee TODO list progress in the exact format:
 Employee EMPLOYEE_NAME is done with
 tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
 """
-import requests
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
+    import requests
 
-i = 0
-name = ""
-total = 0
-complete = 0
+    i = 0
+    name = ""
+    total = 0
+    complete = 0
 
-req = requests.get("https://jsonplaceholder.typicode.com/users",
-                   params={"id": argv[1]})
-name = req.json()[0]["name"]
-req = requests.get("https://jsonplaceholder.typicode.com/todos",
-                   params={"userId": argv[1], "completed": "true"})
-complete = len(req.json())
-req = requests.get("https://jsonplaceholder.typicode.com/todos",
-                   params={"userId": argv[1]})
-total = len(req.json())
-print("Employee {} is done with tasks({:d}/{:d}):".format(
-        name, complete, total))
-while i < len(req.json()):
-    print("\t {}".format(req.json()[i]["title"]))
-    i += 1
+    req = requests.get("https://jsonplaceholder.typicode.com/users",
+                       params={"id": argv[1]})
+    name = req.json()[0].get("name")
+    req = requests.get("https://jsonplaceholder.typicode.com/todos",
+                       params={"userId": argv[1], "completed": "true"})
+    complete = len(req.json())
+    req = requests.get("https://jsonplaceholder.typicode.com/todos",
+                       params={"userId": argv[1]})
+    total = len(req.json())
+    print("Employee {} is done with tasks({:d}/{:d}):".format(
+            name, complete, total))
+    while i < len(req.json()):
+        print("\t {}".format(req.json()[i].get("title")))
+        i += 1
