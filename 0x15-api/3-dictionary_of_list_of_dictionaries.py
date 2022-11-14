b18d854cebcaf3800f6949ff0646354b7208af4e
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-11-14 15:49:33 +0100

    finishing up

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+1. Records all tasks from all employees
+2. File name must be: todo_all_employees.json
+"""
+if __name__ == "__main__":
+    import json
+    import requests
+
+    url_user = "https://jsonplaceholder.typicode.com/users"
+    url_todo = "https://jsonplaceholder.typicode.com/todos"
+    final_dict = {}
+    new_list = []
+    i = 0
+    name = ""
+    req = requests.get(url_user)
+    users = len(req.json())
+
+    while i < users:
+        j = 0
+        new_list = []
+        req = requests.get(url_todo, params={"userId": i})
+        todos = len(req.json())
+        while j < todos:
+            req = requests.get(url_todo, params={"userId": i})
+            title = req.json()[i].get("title")
+            completion = req.json()[i].get("completed")
+            req = requests.get(url_user, params={"id": i})
+            name = req.json()[0].get("username")
+            new_dict = {}
+            new_dict["task"] = title
+            new_dict["completed"] = completion
+            new_dict["username"] = name
+            new_list.append(new_dict)
+            j += 1
+        final_dict["{}".format(i)] = new_list
+        i += 1
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(final_dict, f)
