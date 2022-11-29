46c32a9fa484caa382d019cc6df305e7adb2ee2c
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-11-29 17:11:30 +0100

    trying to figure out why the checker keeps failing me

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import requests
 """
  a recursive function that queries the Reddit API and returns a list
  containing the titles of all hot articles for a given subreddit.
  If no results are found for the given subreddit,
  the function should return None.
 """
+import requests
 
 
 def recurse(subreddit, hot_list=[], count=0, after=""):
@@ -19,6 +19,7 @@ Gecko/geckotrail Firefox/firefoxversion"}
             "after": after,
             "count": count
             }
+
     r = requests.get("https://www.reddit.com/r/{}/hot/.json".format(subreddit),
                      params=params, headers=headers, allow_redirects=False)
     if r.status_code != 200:
@@ -27,7 +28,7 @@ Gecko/geckotrail Firefox/firefoxversion"}
     count += res.get("dist")
     after = res.get("after")
     for child in res.get("children"):
-        hot_list.append(child.get("data").get("title"))
+        #hot_list.append(child.get("data").get("title"))
     if after is None:
         return hot_list
     return recurse(subreddit, hot_list, count, after)
